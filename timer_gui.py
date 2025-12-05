d8b92ee367cd211b50d9786b330eb058154cae48
sam <seriousemail178@gmail.com>
2025-12-05 15:10:48 +1100

    Added some styling

--- timer_gui.py
+++ timer_gui.py
@@ -7,27 +7,53 @@ import timer
 class TimerGUI:
     def __init__(self):
         self.timer = timer.Timer()
-        self.root = Tk()
+
+        self.root = Tk(screenName="Timer")
         self.root.protocol("WM_DELETE_WINDOW", self._close_gui)
 
-        self.frame = ttk.Frame(self.root)
+        self.frame = ttk.Frame(
+            self.root,
+            padding=10
+        )
         self.frame.grid_configure(column=3, row=3)
 
-        self.lbl_title = ttk.Label(self.frame, text="Timer")
+        self.lbl_title = ttk.Label(
+            self.frame, 
+            text="Timer",
+            font="Arial",
+            justify="center",
+            underline=TRUE,
+        )
         self.lbl_title.grid(column=2, row=1)
 
         self.str_time = StringVar(self.frame)
         self._update_time()
-        self.lbl_time = ttk.Label(self.frame, textvariable=self.str_time)
+        self.lbl_time = ttk.Label(
+            self.frame, 
+            textvariable=self.str_time,
+            font="Arial",
+        )
         self.lbl_time.grid(column=2, row=2)
 
-        self.btn_start = ttk.Button(self.frame, text="Start", command=self.start_timer)
+        self.btn_start = ttk.Button(
+            self.frame, 
+            text="Start", 
+            command=self.start_timer
+        )
         self.btn_start.grid(column=1, row=3)
 
-        self.btn_stop = ttk.Button(self.frame, text="Stop", command=self.stop_timer, default="disabled")
+        self.btn_stop = ttk.Button(
+            self.frame, 
+            text="Stop", 
+            command=self.stop_timer
+        )
         self.btn_stop
 
-        self.btn_reset = ttk.Button(self.frame, text="Reset", command=self.reset_timer)
+        self.btn_reset = ttk.Button(
+            self.frame, 
+            text="Reset", 
+            command=self.reset_timer
+        )
         self.btn_reset.grid(column=3, row=3)
 
         self.root.mainloop()
@@ -54,6 +80,7 @@ class TimerGUI:
             self.timer.min,
             self.timer.sec
         ))
+        
         self.root.after(500, self._update_time)
 
     def _close_gui(self):
